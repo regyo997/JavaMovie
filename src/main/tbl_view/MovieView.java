c45240417bc2764dcfdcb8da82def1c629b33554
minalalala <mina1011a@yahoo.com.tw>
2020-05-06 15:57:10 +0800

    add MovieView UserView

---
+++ src/main/tbl_view/MovieView.java
@@ -0,0 +1,62 @@
+package main.tbl_view;
+
+public class MovieView {
+	private String movieId;
+	private String movieName;
+	private String movieRating;
+	private String movieInfo;
+	private String releaseDate;
+	private String runtime;
+	private String directedBy;
+	private String cast;
+	
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public String getMovieRating() {
+		return movieRating;
+	}
+	public void setMovieRating(String movieRating) {
+		this.movieRating = movieRating;
+	}
+	public String getMovieInfo() {
+		return movieInfo;
+	}
+	public void setMovieInfo(String movieInfo) {
+		this.movieInfo = movieInfo;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getRuntime() {
+		return runtime;
+	}
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+	public String getDirectedBy() {
+		return directedBy;
+	}
+	public void setDirectedBy(String directedBy) {
+		this.directedBy = directedBy;
+	}
+	public String getCast() {
+		return cast;
+	}
+	public void setCast(String cast) {
+		this.cast = cast;
+	}
+
+}
