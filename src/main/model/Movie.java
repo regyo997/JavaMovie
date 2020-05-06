f423eab1d618faebfe547a9448b882ece31076bb
minalalala <mina1011a@yahoo.com.tw>
2020-05-06 15:59:09 +0800

    add User.java, edit Movie.java ShowTime.java

--- src/main/model/Movie.java
+++ src/main/model/Movie.java
@@ -4,31 +4,41 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.ArrayList;
+
+import main.tbl_view.MovieView;
 
 public class Movie {
 	private Connection conn = null;
 	private Statement stmt = null;
 	private ResultSet result = null;
 	private String sql = "USE JAVA_THEATER; SELECT * FROM TBLMOVIE;";
-	private HashMap<String,String> movieId_name = new HashMap<String,String>();
+	private ArrayList<MovieView> movieViews = new ArrayList<MovieView>();
+	private MovieView movieView = null;
 	
-	public HashMap<String,String> getMovieName(){
+	public ArrayList<MovieView> getMovieViews(){
 		try {
 			conn = ConnectionManager.getConnection();
 			stmt = conn.createStatement();
 			result = stmt.executeQuery(sql);
 			
 			while(result.next()) {
-				movieId_name.put(result.getString(1), result.getString(2));
+				movieView = new MovieView();
+				movieView.setMovieId(result.getString(1));
+				movieView.setMovieName(result.getString(2));
+				movieView.setMovieRating(result.getString(3));
+				movieView.setMovieInfo(result.getString(4));
+				movieView.setReleaseDate(result.getString(5));
+				movieView.setRuntime(result.getString(6));
+				movieView.setDirectedBy(result.getString(7));
+				movieView.setCast(result.getString(8));
+				movieViews.add(movieView);
 			}
 			
 			conn.close();
 		}catch(SQLException e) {}
 		
-		return movieId_name;
+		return movieViews;
 	}
 
 }
