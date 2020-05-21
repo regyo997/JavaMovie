57bebfe2e07dd52f1858b4a7be2982045c94fac6
minalalala <mina1011a@yahoo.com.tw>
2020-05-21 22:54:56 +0800

    取下周四前的場次

--- src/main/model/ShowTime.java
+++ src/main/model/ShowTime.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedHashMap;
 
@@ -13,12 +14,25 @@ public class ShowTime {
 	private Connection conn = null;
 	private Statement stmt = null;
 	private ResultSet result = null;
-	//todo: 只抓下周四前的資料
-	private String sql = "USE JAVA_THEATER; SELECT DISTINCT MOVIE_ID,SHOWTIME FROM TBLSHOWTIMES WHERE SHOWTIME BETWEEN CONVERT(DATE, GETDATE()) AND CONVERT(DATE, GETDATE()+3)  ORDER BY MOVIE_ID,SHOWTIME;";
+	
 	private LinkedHashMap<String,ArrayList<String>> movieId_time = new LinkedHashMap<String,ArrayList<String>>();
 	private ArrayList<String> showtimes = null;
 	
 	public LinkedHashMap<String,ArrayList<String>> getShowTimeData() {
+		Calendar cal = Calendar.getInstance();
+		int today = cal.get(Calendar.DAY_OF_WEEK);
+		int days = 0;
+		
+		if(today == 6 || today == 7)
+			days = (7-today)+6;
+		else
+			days = 6-today;
+		// sql view
+		String sql = "USE JAVA_THEATER; "
+				+ "SELECT MOVIE_ID,SHOWTIME, COUNT(*) FROM TBLSHOWTIMES "
+				+ "WHERE SHOWTIME BETWEEN CONVERT(DATE, GETDATE()) AND CONVERT(DATE, GETDATE()+ "+days+")  "
+				+ "GROUP BY MOVIE_ID,SHOWTIME ORDER BY MOVIE_ID,SHOWTIME;";
+		
 		try {
 			conn = ConnectionManager.getConnection();
 			stmt = conn.createStatement();
