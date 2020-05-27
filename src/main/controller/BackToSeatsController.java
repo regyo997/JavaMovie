b518be4e028bb85cc555bbd312502214b1e62ee7
minalalala <mina1011a@yahoo.com.tw>
2020-05-27 17:59:31 +0800

    add BackToSeatsController.java

---
+++ src/main/controller/BackToSeatsController.java
@@ -0,0 +1,51 @@
+package main.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import main.model.Movie;
+import main.model.ShowTime;
+
+/**
+ * Servlet implementation class BackToSeatsController
+ */
+@WebServlet("/back")
+public class BackToSeatsController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String movieId = request.getParameter("movieId");
+		String movieName = new Movie().getMovieById(movieId).getMovieName();
+		String showtime = request.getParameter("showtime");
+		String seats = request.getParameter("seats");
+		String userId = request.getParameter("userId");
+		
+		ShowTime show = new ShowTime();
+		String[] seat = seats.split(",");
+		for (int i=0;i<seat.length;i++) {
+			String[] row_col = seat[i].split("_");
+			
+			String row = row_col[0].replaceAll("[\\pP\\p{Punct}]","").trim();
+			int col = Integer.parseInt(row_col[1].replaceAll("[\\pP\\p{Punct}]",""));
+			
+			show.changeSeatsStatus(movieId, showtime, row, col, userId,"N");
+		}
+		
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
