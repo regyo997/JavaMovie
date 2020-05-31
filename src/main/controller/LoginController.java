4c99c3a30832dd70184c7b6cd3df2158f838d26d
racing888899 <racing888899@gmail.com>
2020-05-31 21:25:49 +0800

    不用進修改也能抓到userinfo了

--- src/main/controller/LoginController.java
+++ src/main/controller/LoginController.java
@@ -1,7 +1,6 @@
 package main.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -22,23 +21,28 @@ public class LoginController extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-		HttpSession session=request.getSession();
-		PrintWriter out=response.getWriter();
+		HttpSession session = request.getSession();
 
 		Login login = new Login();
 		String name = login.signin(request.getParameter("USER_ID"), request.getParameter("PASSWORD"));
-		String state= login.getState();
-		if (name!=null &&state.equals("1")) {
+		String state = login.getState();
+	
+		if (name != null && state.equals("1")) {
 			session.setAttribute("login_success", name);
+			UserQuery userquery = new UserQuery();
+			UserView user_info = userquery.getUserDatas(request.getParameter("USER_ID"));
+			session.setAttribute("user_info", user_info);
 			session.setAttribute("USER_ID", request.getParameter("USER_ID"));
 			request.getRequestDispatcher("index.jsp").forward(request, response);
-		} else if (name==null) {
+		} else if (name == null) {
 			session.setAttribute("login_failed", "error");
-			//out.print("<script> alert('登入失敗，帳號或密碼錯誤');window.location='login.jsp'</script>");
+			// out.print("<script>
+			// alert('登入失敗，帳號或密碼錯誤');window.location='login.jsp'</script>");
 			request.getRequestDispatcher("login.jsp").forward(request, response);
-		} else if(name!=null && state.equals("0")) {
+		} else if (name != null && state.equals("0")) {
 			session.setAttribute("login_failed", "disable");
-			//out.print("<script> alert('帳號尚未啟用，請至email開啟認證信啟用帳號');window.location='login.jsp'</script>");
+			// out.print("<script>
+			// alert('帳號尚未啟用，請至email開啟認證信啟用帳號');window.location='login.jsp'</script>");
 			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
 	}
