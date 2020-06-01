1e5d46d0fe9f2c5640a40c1c66068d133eb6258a
minalalala <mina1011a@yahoo.com.tw>
2020-06-01 09:08:44 +0800

    TBLTICKET_INFO 新增三個欄位

--- src/main/model/TicketInfo.java
+++ src/main/model/TicketInfo.java
@@ -8,10 +8,10 @@ public class TicketInfo {
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
 
-	public void setTicketInfo(String movieId, String showtime, String type, String userId, String transactionNo) {
+	public void setTicketInfo(String movieId, String showtime, String hall, String row, int no, String type, String userId, String transactionNo) {
 		String sql="USE JAVA_THEATER;"
-				+ "INSERT INTO TBLTICKET_INFO(MOVIE_ID,SHOWTIME,TICKET_TYPE,SALES_ID,USER_ID,TRANSACTION_NO) "
-				+ "VALUES(?,?,?,0,?,?);";
+				+ "INSERT INTO TBLTICKET_INFO(MOVIE_ID,SHOWTIME,HALL,ROW,NO,TICKET_TYPE,SALES_ID,USER_ID,TRANSACTION_NO) "
+				+ "VALUES(?,?,?,?,?,?,0,?,?);";
 		
 		try {
 			conn = ConnectionManager.getConnection();
@@ -19,9 +19,12 @@ public class TicketInfo {
 			
 			pstmt.setString(1, movieId);
 			pstmt.setString(2, showtime);
-			pstmt.setString(3, type);
-			pstmt.setString(4, userId);
-			pstmt.setString(5, transactionNo);
+			pstmt.setString(3, hall);
+			pstmt.setString(4, row);
+			pstmt.setInt(5, no);
+			pstmt.setString(6, type);
+			pstmt.setString(7, userId);
+			pstmt.setString(8, transactionNo);
 			
 			if(pstmt.executeUpdate() != 0) {
 				System.out.println("TBLTICKET_INFO insert success!");
